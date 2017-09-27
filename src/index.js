9ebed9087c0941eb23438c5b7e52c4da461f7b70
Kevin <kevinmoore9@users.noreply.github.com>
2017-09-26 22:46:12 -0700

    authenticate and fetchJobs actions

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { fetchJob, fetchJobs } from './home/actions';
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
+window.fetchJob = fetchJob;
+window.fetchJobs = fetchJobs;
