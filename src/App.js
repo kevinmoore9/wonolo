666936990e0511a77046ffb66b8917b16240d792
Kevin <kevinmoore9@users.noreply.github.com>
2017-09-27 00:20:11 -0700

    get initial state root component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,49 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { getAuthToken, getJobs } from './app/util';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      authToken: null,
+      jobs: null
+    }
+  }
+  async authenticate() {
+    let token;
+    try {
+      token = await getAuthToken();
+    } catch(e) {
+      console.log(e);
+    }
+
+    this.setState({authToken: token});
+    this.fetchJobs({token: token});
+  }
+  async fetchJobs(params) {
+    let jobs;
+    try {
+      jobs = await getJobs(params);
+    } catch(e) {
+      console.log(e);
+    }
+    this.setState({jobs: jobs});
+  }
+
+  componentWillMount() {
+    this.authenticate();
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.jobs === null) {
+      return false;
+    }
+    return true;
+  }
+
+
   render() {
     return (
       <div className="App">
---
+++ src/app/util.js
@@ -0,0 +1,27 @@
+export const getAuthToken = () => (
+  fetch('http://api.wonolo.com/api_v2/authenticate?api_key=pk_live_x4jAaDx1kQykKKzTp2fg&secret_key=sk_live_kDfup8NWU8dJ8KoAHrT2', {
+    method: 'POST',
+  }).then(res => {
+    return res.text();
+  }).then(txt => {
+    return JSON.parse(txt).token;
+  })
+)
+
+export const getJobs = (params) => (
+  fetch(createQueryString('http://api.wonolo.com/api_v2/jobs', params), {
+    method: 'GET',
+  }).then(res => {
+    return res.text();
+  }).then(txt => {
+    return JSON.parse(txt);
+  })
+)
+
+function createQueryString(url, params) {
+  url += "?";
+  Object.keys(params).forEach(key => {
+    url += (`${key}=${params[key]}`);
+  })
+  return url;
+}
